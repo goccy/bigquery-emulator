3d67c9324728826787ba12679a91c467538c66e6
Masaaki Goshima <goccy54@gmail.com>
2022-11-07 19:41:27 +0900

    refactor storage_handler.go

--- server/storage_handler.go
+++ server/storage_handler.go
@@ -9,8 +9,10 @@ import (
 	"time"
 
 	"github.com/goccy/bigquery-emulator/internal/logger"
+	internaltypes "github.com/goccy/bigquery-emulator/internal/types"
 	"github.com/goccy/bigquery-emulator/types"
 	goavro "github.com/linkedin/goavro/v2"
+	bigqueryv2 "google.golang.org/api/bigquery/v2"
 	storagepb "google.golang.org/genproto/googleapis/cloud/bigquery/storage/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -35,59 +37,26 @@ type streamStatus struct {
 	schemaText    string
 }
 
+type AVROSchema struct {
+	ReadSessionSchema *storagepb.ReadSession_AvroSchema
+	Schema            *types.AVROSchema
+	Text              string
+}
+
 func (s *storageReadServer) CreateReadSession(ctx context.Context, req *storagepb.CreateReadSessionRequest) (*storagepb.ReadSession, error) {
-	sessionName := fmt.Sprintf("%s/locations/%s/sessions/%s", req.Parent, "location", "session")
-	paths := strings.Split(req.ReadSession.Table, "/")
-	if len(paths)%2 != 0 {
-		return nil, fmt.Errorf("unexpected table path: %s", req.ReadSession.Table)
-	}
-	var (
-		projectID string
-		datasetID string
-		tableID   string
-	)
-	for i := 0; i < len(paths); i += 2 {
-		switch paths[i] {
-		case "projects":
-			projectID = paths[i+1]
-		case "datasets":
-			datasetID = paths[i+1]
-		case "tables":
-			tableID = paths[i+1]
-		}
-	}
-	if projectID == "" {
-		return nil, fmt.Errorf("unspecified project id")
-	}
-	if datasetID == "" {
-		return nil, fmt.Errorf("unspecified dataset id")
-	}
-	if tableID == "" {
-		return nil, fmt.Errorf("unspecified table id")
-	}
-	project, err := s.server.metaRepo.FindProject(ctx, projectID)
+	sessionID := randomID()
+	sessionName := fmt.Sprintf("%s/locations/%s/sessions/%s", req.Parent, "location", sessionID)
+	projectID, datasetID, tableID, err := s.getIDsFromPath(req.ReadSession.Table)
 	if err != nil {
 		return nil, err
 	}
-	dataset := project.Dataset(datasetID)
-	if dataset == nil {
-		return nil, fmt.Errorf("dataset %s is not found in project %s", datasetID, projectID)
-	}
-	table := dataset.Table(tableID)
-	if table == nil {
-		return nil, fmt.Errorf("table %s is not found in dataset %s", tableID, datasetID)
-	}
-	tableMetadata, err := new(tablesGetHandler).Handle(ctx, &tablesGetRequest{
-		server:  s.server,
-		project: project,
-		dataset: dataset,
-		table:   table,
-	})
+	tableMetadata, err := s.getTableMetadata(ctx, projectID, datasetID, tableID)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get table metadata: %w", err)
 	}
 	streams := make([]*storagepb.ReadStream, 0, req.MaxStreamCount)
-	streamName := fmt.Sprintf("%s/streams/stream", sessionName)
+	streamID := randomID()
+	streamName := fmt.Sprintf("%s/streams/%s", sessionName, streamID)
 	for i := int32(0); i < req.MaxStreamCount; i++ {
 		streams = append(streams, &storagepb.ReadStream{
 			Name: streamName,
@@ -106,34 +75,27 @@ func (s *storageReadServer) CreateReadSession(ctx context.Context, req *storagep
 	}
 	outputColumns := req.ReadSession.ReadOptions.SelectedFields
 	condition := req.ReadSession.ReadOptions.RowRestriction
-	var (
-		avroSchema *types.AVROSchema
-		schemaText string
-	)
+	outputColumnMap := map[string]struct{}{}
+	for _, outputColumn := range outputColumns {
+		outputColumnMap[outputColumn] = struct{}{}
+	}
+	status := &streamStatus{
+		projectID:     projectID,
+		datasetID:     datasetID,
+		tableID:       tableID,
+		outputColumns: outputColumns,
+		condition:     condition,
+		dataFormat:    readSession.DataFormat,
+	}
 	switch readSession.DataFormat {
 	case storagepb.DataFormat_AVRO:
-		avroSchema = types.TableToAVRO(tableMetadata)
-		if len(outputColumns) != 0 {
-			outputColumnMap := map[string]struct{}{}
-			for _, outputColumn := range outputColumns {
-				outputColumnMap[outputColumn] = struct{}{}
-			}
-			filteredFields := make([]*types.AVROFieldSchema, 0, len(avroSchema.Fields))
-			for _, field := range avroSchema.Fields {
-				if _, exists := outputColumnMap[field.Name]; exists {
-					filteredFields = append(filteredFields, field)
-				}
-			}
-			avroSchema.Fields = filteredFields
-		}
-		schema, err := json.Marshal(avroSchema)
+		schema, err := s.getAVROSchema(tableMetadata, outputColumnMap)
 		if err != nil {
 			return nil, err
 		}
-		schemaText = string(schema)
-		readSession.Schema = &storagepb.ReadSession_AvroSchema{
-			AvroSchema: &storagepb.AvroSchema{Schema: schemaText},
-		}
+		readSession.Schema = schema.ReadSessionSchema
+		status.avroSchema = schema.Schema
+		status.schemaText = schema.Text
 	case storagepb.DataFormat_ARROW:
 		readSession.Schema = &storagepb.ReadSession_ArrowSchema{
 			ArrowSchema: &storagepb.ArrowSchema{SerializedSchema: nil},
@@ -142,101 +104,29 @@ func (s *storageReadServer) CreateReadSession(ctx context.Context, req *storagep
 		return nil, fmt.Errorf("unexpected data format %s", readSession.DataFormat)
 	}
 	s.mu.Lock()
-	s.streamMap[streamName] = &streamStatus{
-		projectID:     projectID,
-		datasetID:     datasetID,
-		tableID:       tableID,
-		outputColumns: outputColumns,
-		condition:     condition,
-		dataFormat:    readSession.DataFormat,
-		avroSchema:    avroSchema,
-		schemaText:    schemaText,
-	}
+	s.streamMap[streamName] = status
 	s.mu.Unlock()
 	return readSession, nil
 }
 
 func (s *storageReadServer) ReadRows(req *storagepb.ReadRowsRequest, stream storagepb.BigQueryRead_ReadRowsServer) error {
 	s.mu.RLock()
-	streamStatus := s.streamMap[req.ReadStream]
+	status := s.streamMap[req.ReadStream]
 	s.mu.RUnlock()
 
-	if streamStatus == nil {
+	if status == nil {
 		return fmt.Errorf("failed to find stream status from %s", req.ReadStream)
 	}
-	var columns string
-	if len(streamStatus.outputColumns) != 0 {
-		outputColumns := make([]string, len(streamStatus.outputColumns))
-		for idx, outputColumn := range streamStatus.outputColumns {
-			outputColumns[idx] = fmt.Sprintf("`%s`", outputColumn)
-		}
-		columns = strings.Join(outputColumns, ",")
-	} else {
-		columns = "*"
-	}
-	var condition string
-	if streamStatus.condition != "" {
-		condition = fmt.Sprintf("WHERE %s", streamStatus.condition)
-	}
-	query := fmt.Sprintf("SELECT %s FROM `%s` %s", columns, streamStatus.tableID, condition)
-
 	ctx := context.Background()
 	ctx = logger.WithLogger(ctx, s.server.logger)
 
-	conn, err := s.server.connMgr.Connection(ctx, streamStatus.projectID, streamStatus.datasetID)
-	if err != nil {
-		return fmt.Errorf("failed to get connection: %w", err)
-	}
-	tx, err := conn.Begin(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to start transaction: %w", err)
-	}
-	defer tx.RollbackIfNotCommitted()
-
-	response, err := s.server.contentRepo.Query(
-		ctx,
-		tx,
-		streamStatus.projectID,
-		streamStatus.datasetID,
-		query,
-		nil,
-	)
+	response, err := s.query(ctx, status)
 	if err != nil {
 		return err
 	}
-	switch streamStatus.dataFormat {
+	switch status.dataFormat {
 	case storagepb.DataFormat_AVRO:
-		codec, err := goavro.NewCodec(streamStatus.schemaText)
-		if err != nil {
-			return err
-		}
-		var buf []byte
-		for _, row := range response.Rows {
-			value, err := row.AVROValue(streamStatus.avroSchema.Fields)
-			if err != nil {
-				return err
-			}
-			b, err := codec.BinaryFromNative(buf, value)
-			if err != nil {
-				return err
-			}
-			buf = b
-		}
-		rows := &storagepb.ReadRowsResponse_AvroRows{
-			AvroRows: &storagepb.AvroRows{
-				SerializedBinaryRows: buf,
-				RowCount:             int64(response.TotalRows),
-			},
-		}
-		if err := stream.Send(&storagepb.ReadRowsResponse{
-			Rows:     rows,
-			RowCount: int64(response.TotalRows),
-			Schema: &storagepb.ReadRowsResponse_AvroSchema{
-				AvroSchema: &storagepb.AvroSchema{
-					Schema: streamStatus.schemaText,
-				},
-			},
-		}); err != nil {
+		if err := s.sendAVRORows(status, response, stream); err != nil {
 			return err
 		}
 	case storagepb.DataFormat_ARROW:
@@ -247,6 +137,161 @@ func (s *storageReadServer) ReadRows(req *storagepb.ReadRowsRequest, stream stor
 
 func (s *storageReadServer) SplitReadStream(ctx context.Context, req *storagepb.SplitReadStreamRequest) (*storagepb.SplitReadStreamResponse, error) {
 	return nil, fmt.Errorf("failed to split read stream !!!!")
+}
+
+func (s *storageReadServer) getIDsFromPath(path string) (string, string, string, error) {
+	paths := strings.Split(path, "/")
+	if len(paths)%2 != 0 {
+		return "", "", "", fmt.Errorf("unexpected table path: %s", path)
+	}
+	var (
+		projectID string
+		datasetID string
+		tableID   string
+	)
+	for i := 0; i < len(paths); i += 2 {
+		switch paths[i] {
+		case "projects":
+			projectID = paths[i+1]
+		case "datasets":
+			datasetID = paths[i+1]
+		case "tables":
+			tableID = paths[i+1]
+		}
+	}
+	if projectID == "" {
+		return "", "", "", fmt.Errorf("unspecified project id")
+	}
+	if datasetID == "" {
+		return "", "", "", fmt.Errorf("unspecified dataset id")
+	}
+	if tableID == "" {
+		return "", "", "", fmt.Errorf("unspecified table id")
+	}
+	return projectID, datasetID, tableID, nil
+}
+
+func (s *storageReadServer) getTableMetadata(ctx context.Context, projectID, datasetID, tableID string) (*bigqueryv2.Table, error) {
+	project, err := s.server.metaRepo.FindProject(ctx, projectID)
+	if err != nil {
+		return nil, err
+	}
+	dataset := project.Dataset(datasetID)
+	if dataset == nil {
+		return nil, fmt.Errorf("dataset %s is not found in project %s", datasetID, projectID)
+	}
+	table := dataset.Table(tableID)
+	if table == nil {
+		return nil, fmt.Errorf("table %s is not found in dataset %s", tableID, datasetID)
+	}
+	return new(tablesGetHandler).Handle(ctx, &tablesGetRequest{
+		server:  s.server,
+		project: project,
+		dataset: dataset,
+		table:   table,
+	})
+}
+
+func (s *storageReadServer) getAVROSchema(tableMetadata *bigqueryv2.Table, outputColumnMap map[string]struct{}) (*AVROSchema, error) {
+	avroSchema := types.TableToAVRO(tableMetadata)
+	if len(outputColumnMap) != 0 {
+		filteredFields := make([]*types.AVROFieldSchema, 0, len(avroSchema.Fields))
+		for _, field := range avroSchema.Fields {
+			if _, exists := outputColumnMap[field.Name]; exists {
+				filteredFields = append(filteredFields, field)
+			}
+		}
+		avroSchema.Fields = filteredFields
+	}
+	schema, err := json.Marshal(avroSchema)
+	if err != nil {
+		return nil, err
+	}
+	schemaText := string(schema)
+	return &AVROSchema{
+		ReadSessionSchema: &storagepb.ReadSession_AvroSchema{
+			AvroSchema: &storagepb.AvroSchema{Schema: schemaText},
+		},
+		Schema: avroSchema,
+		Text:   schemaText,
+	}, nil
+}
+
+func (s *storageReadServer) buildQuery(status *streamStatus) string {
+	var columns string
+	if len(status.outputColumns) != 0 {
+		outputColumns := make([]string, len(status.outputColumns))
+		for idx, outputColumn := range status.outputColumns {
+			outputColumns[idx] = fmt.Sprintf("`%s`", outputColumn)
+		}
+		columns = strings.Join(outputColumns, ",")
+	} else {
+		columns = "*"
+	}
+	var condition string
+	if status.condition != "" {
+		condition = fmt.Sprintf("WHERE %s", status.condition)
+	}
+	return fmt.Sprintf("SELECT %s FROM `%s` %s", columns, status.tableID, condition)
+}
+
+func (s *storageReadServer) query(ctx context.Context, status *streamStatus) (*internaltypes.QueryResponse, error) {
+	conn, err := s.server.connMgr.Connection(ctx, status.projectID, status.datasetID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get connection: %w", err)
+	}
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start transaction: %w", err)
+	}
+	defer tx.RollbackIfNotCommitted()
+
+	query := s.buildQuery(status)
+	return s.server.contentRepo.Query(
+		ctx,
+		tx,
+		status.projectID,
+		status.datasetID,
+		query,
+		nil,
+	)
+}
+
+func (s *storageReadServer) sendAVRORows(status *streamStatus, response *internaltypes.QueryResponse, stream storagepb.BigQueryRead_ReadRowsServer) error {
+	codec, err := goavro.NewCodec(status.schemaText)
+	if err != nil {
+		return fmt.Errorf("failed to create avro codec from schema %s: %w", status.schemaText, err)
+	}
+	var buf []byte
+	for _, row := range response.Rows {
+		value, err := row.AVROValue(status.avroSchema.Fields)
+		if err != nil {
+			return fmt.Errorf("failed to convert response fields to avro value: %w", err)
+		}
+		b, err := codec.BinaryFromNative(buf, value)
+		if err != nil {
+			return fmt.Errorf("failed to encode binary from go value: %w", err)
+		}
+		buf = b
+	}
+	rows := &storagepb.ReadRowsResponse_AvroRows{
+		AvroRows: &storagepb.AvroRows{
+			SerializedBinaryRows: buf,
+			RowCount:             int64(response.TotalRows),
+		},
+	}
+	if err := stream.Send(&storagepb.ReadRowsResponse{
+		Rows:     rows,
+		RowCount: int64(response.TotalRows),
+		Schema: &storagepb.ReadRowsResponse_AvroSchema{
+			AvroSchema: &storagepb.AvroSchema{
+				Schema: status.schemaText,
+			},
+		},
+	}); err != nil {
+		return fmt.Errorf("failed to send read rows response for avro format: %w", err)
+	}
+	return nil
 }
 
 func registerStorageServer(grpcServer *grpc.Server, srv *Server) {
